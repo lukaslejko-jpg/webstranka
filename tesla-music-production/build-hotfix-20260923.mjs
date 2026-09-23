f45e9b0f77c2edbb0a99b8589eb5eaad1472071e
lukaslejko-jpg <lukaslejko@gmail.com>
2026-09-23 21:51:17 +0200

    build(tesla-music): override fixed live-search layout

--- tesla-music-production/build-hotfix-20260923.mjs
+++ tesla-music-production/build-hotfix-20260923.mjs
@@ -26,3 +26,4 @@ console.log('TESLA_MUSIC_HOTFIX: immutable production snapshot + one mobile play
 await fs.copyFile('mode-v9.js','public/mode-v9.js');
 await fs.copyFile('desktop-v97-youtube-search.js','public/desktop-v97-youtube-search.js');
 await fs.copyFile('member-v50-foryou.js','public/member-v50-foryou.js');
+await fs.copyFile('member-v49-search-layout.js','public/member-v49-search-layout.js');
